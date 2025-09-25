7791aa5ff303ff29f3497961b9942e8991bbc46f
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-09-25 21:04:49 +0600

    refactor with middleware and add path.resolve( )

--- backend/src/middleware/index.js
+++ backend/src/middleware/index.js
@@ -1,21 +1,51 @@
+// const express = require("express");
+// const cors = require("cors");
+// const morgan = require("morgan");
+// const swaggerUI = require("swagger-ui-express");
+// const YAML = require("yamljs");
+// const OpenApiValidator = require("express-openapi-validator");
+// const swaggerDoc = YAML.load("./swagger.yaml");
+// // const authenticate = require('./authenticate')
+
+// const applyMiddleware = (app) => {
+//   app.use(cors());
+//   app.use(morgan("dev"));
+//   app.use(express.json());
+//   app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
+
+//   app.use(
+//     OpenApiValidator.middleware({
+//       apiSpec: "./swagger.yaml",
+//     })
+//   );
+// };
+
+// module.exports = applyMiddleware;
+
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const swaggerUI = require("swagger-ui-express");
 const YAML = require("yamljs");
 const OpenApiValidator = require("express-openapi-validator");
-const swaggerDoc = YAML.load("./swagger.yaml");
-// const authenticate = require('./authenticate')
+const path = require("path");
+
+// Resolve absolute path for swagger.yaml
+const swaggerPath = path.resolve(process.cwd(), "swagger.yaml");
+const swaggerDoc = YAML.load(swaggerPath);
 
 const applyMiddleware = (app) => {
   app.use(cors());
   app.use(morgan("dev"));
   app.use(express.json());
+
+  // Swagger UI
   app.use("/docs", swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 
+  // OpenAPI validator
   app.use(
     OpenApiValidator.middleware({
-      apiSpec: "./swagger.yaml",
+      apiSpec: swaggerPath,
     })
   );
 };
