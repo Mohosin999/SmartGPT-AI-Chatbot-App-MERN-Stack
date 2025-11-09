f193ede12cb6e477c52e4ecbbb78f9952441a9fd
Mohosin Hasan Akash <107780771+Mohosin999@users.noreply.github.com>
2025-11-09 07:24:48 +0600

    Update index.js

--- backend/src/lib/token/index.js
+++ backend/src/lib/token/index.js
@@ -7,7 +7,7 @@ const generateAccessToken = ({
   payload,
   algorithm = "HS256",
   secret = process.env.ACCESS_TOKEN_SECRET,
-  expiresIn = "15m",
+  expiresIn = "2m",
 }) => {
   try {
     return jwt.sign(payload, secret, { expiresIn, algorithm });
@@ -19,7 +19,8 @@ const generateAccessToken = ({
 
 const generateRefreshToken = () => {
   const refreshToken = uuidv4();
-  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days
+  // const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // 7 days
+  const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 7 days
   return { refreshToken, expiresAt };
 };
 
